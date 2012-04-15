7b072d349fe127227c4c81a3accc5cb4e2b564aa
jimbray16@gmail.com <jimbray16@gmail.com@2bd18733-4cb9-98e4-dfe0-e8d2d042db61>
2012-04-15 23:31:24 +0000

    准备获取网络数据

--- trunk/src/com/bullshite/MainActivity.java
+++ trunk/src/com/bullshite/MainActivity.java
@@ -2,12 +2,33 @@ package com.bullshite;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends Activity {
+	private TextView mTvContent = null;
+	private String result = null;
+	
+	private static String URL = "http://code.google.com/p/daroon-player/source/browse/trunk/stream_list_1.1.0_cn.xml";
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        try {
+			result = getHtml(URL, "UTF-8");
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+    }
+    
+    private String getHtml(String url, String encoding) throws MalformedURLException {
+    	URL path = new URL(url);
+    	return null;
     }
 }
