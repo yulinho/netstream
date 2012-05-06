f565643fde0ff68f4df042ae84968ddf581deb3a
jimbray16@gmail.com <jimbray16@gmail.com@2bd18733-4cb9-98e4-dfe0-e8d2d042db61>
2012-05-06 11:43:07 +0000

    更改列表背景

--- trunk/gen/com/bullshite/R.java
+++ trunk/gen/com/bullshite/R.java
@@ -12,6 +12,11 @@ public final class R {
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
+        public static final int item_bg=0x7f020001;
+        public static final int list_item_bg=0x7f020002;
+        public static final int list_item_bg_selector=0x7f020003;
+        public static final int list_selector=0x7f020004;
+        public static final int selector=0x7f020005;
     }
     public static final class id {
         public static final int loading=0x7f050000;
